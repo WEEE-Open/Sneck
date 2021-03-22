85dfed26df35e1c786f2f820b41777a95567f341
alecello <alessandro.celoria@hotmail.com>
2021-03-22 19:47:39 +0100

    Added rudimental Deck class
    Class stores structured data from deck. This stage is still very rudimentary, with clumsy implementation just to pin the workflow down

---
+++ sneck/Deck.py
@@ -0,0 +1,291 @@
+import requests
+import json
+import os
+import logging
+
+from typing import Optional
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+
+
+class DeckUser:
+    def __init__(self, key: str, uid: str, utype: int, name: str):
+        self.__pkey = key     # Used as index in dictionary
+        self.__uuid = uid
+        self.__type = utype
+
+        self.name = name
+
+
+class DeckAcl:
+    def __init__(self, aid: int, user: DeckUser, atype: int, edit: bool, share: bool, manage: bool, owner: bool):
+        # Internal ID of ACL
+        self.__id = aid
+
+        # User this refers to
+        self.user = user
+        self.type = atype
+
+        # If the user is the owner of the entity
+        self.is_owner = owner
+
+        # Access rights
+        self.can_edit = edit
+        self.can_share = share
+        self.can_manage = manage
+
+
+class DeckCard:
+    def __init__(self, cid: int, etag: str, title: str, descr: str, labels: list, ctype: int, attachments: list,
+                 create: datetime, modify: datetime, delete: datetime, target: datetime, assignees: list,
+                 creator: DeckUser, editor: DeckUser, order: int, archived: bool, unread_comments: int, overdue: int):
+        # Internal card id and entity tag
+        self.__id = cid
+        self.__tag = etag
+
+        # Card information
+        self.title = title
+        self.description = descr
+        self.labels = labels
+        self.type = ctype
+        self.attachments = attachments
+
+        # Timestamps
+        self.create_time = create
+        self.modify_time = modify
+        self.delete_time = delete
+        self.target_time = target   # Due date of the card
+
+        # Users
+        self.assignees = assignees
+        self.creator = creator
+        self.last_editor = editor
+
+        # Metadata
+        self.order = order
+        self.archived = archived
+        self.unread_comments = unread_comments
+        self.overdue = overdue
+
+
+class DeckStack:
+    def __init__(self, sid: int, tag: str, modify: datetime, order: int, title: str, cards: list):
+        # Internal stack id and entity tag
+        self.__id = sid
+        self.__etag = tag
+
+        # Stack metadata
+        self.modify_time = modify
+        self.order = order
+
+        # Stack information
+        self.title = title
+        self.cards = cards
+
+
+class DeckBoard:
+    def __init__(self, bid: int, tag: str, perm: list, stacks: list, cal: bool, title: str, shared: bool, acl: list,
+                 archived: bool, col: str, lbls: list, own: DeckUser, delete: datetime, modify: datetime, notify: str):
+        # Internal board id and entity tag
+        self.__id = bid
+        self.__etag = tag
+
+        # Current user access permissions
+        self.__read_permission = perm[0]
+        self.__edit_permission = perm[1]
+        self.__manage_permission = perm[2]
+        self.__share_permission = perm[3]
+
+        # Internal board settings
+        self.__notifications_on = notify
+        self.__calendar_integration = cal
+
+        # Board information (visible as attribute)
+        self.title = title
+        self.color = col
+        self.labels = lbls
+
+        # Users of the board (people that have access)
+        self.owner = own
+        self.acl = acl
+
+        # Properties of the board
+        self.is_archived = archived     # If it has been archived by user
+        self.is_shared = shared         # if it has been shared TO the user
+
+        # Last edit and deletion times (deletion time is 0 if it has not been deleted)
+        self.delete_time = delete
+        self.modify_time = modify
+
+        self.stacks = stacks
+
+    def is_deleted(self) -> bool:
+        return self.delete_time is not None
+
+
+class Deck:
+    def __init__(self, domain: str, username: str, password: str, secure: bool):
+        self.__api_base = f'{"https" if secure else "http"}://{domain}/index.php/apps/deck/api/v1.0/'
+        self.__username = username
+        self.__password = password
+
+        self.next_event = None
+        self.boards = {}
+        self.users = {}
+
+        self.download()
+
+    def __api_request(self, api_bindpost: str) -> json:
+        logging.debug(f'Making request {self.__api_base + api_bindpost}...')
+        response = requests.get(self.__api_base + api_bindpost,
+                                headers={'OCS-APIRequest': 'true', 'Content-Type': 'application/json'},
+                                auth=HTTPBasicAuth(self.__username, self.__password))
+
+        if not response.ok:
+            logging.error(f'Request failed [Return code: {response.status_code}]!')
+            logging.error(f'Unable to download deck content from API {self.__api_base + api_bindpost}')
+
+            return
+        print(response.text)
+        return response.text
+
+    def __is_outdated(self, title: str, timestamp: int):
+        if self.boards is None:
+            return True
+
+        if title in self.boards and self.boards[title].last_edit == timestamp:
+            return False
+        else:
+            return True
+
+    def __parse_user(self, user_json: dict) -> DeckUser:
+        index = user_json if isinstance(user_json, str) else user_json['primaryKey']
+        if index not in self.users:
+            pkey = index
+            uuid = user_json['uid']
+            nick = user_json['displayname']
+            utype = user_json['type']
+
+            self.users[pkey] = DeckUser(pkey, uuid, utype, nick)
+        return self.users[index]
+
+    def __parse_acls(self, acls_json) -> list:
+        return [self.__parse_acl(acl_json) for acl_json in acls_json]
+
+    def __parse_acl(self, acl_json: dict):
+        user = self.__parse_user(acl_json['participant'])
+        owner = acl_json['owner']
+        edit = acl_json['owner']
+        share = acl_json['owner']
+        manage = acl_json['owner']
+        atype = acl_json['type']
+        aid = acl_json['id']
+        return DeckAcl(aid, user, atype, edit, share, manage, owner)
+
+    def __parse_card(self, data: dict):
+        cid = data['id']
+        tag = data['ETag']
+        title = data['title']
+        descr = data['description']
+        labels = data['labels']
+        ctype = data['type']
+        attachments = data['attachments']
+
+        create = datetime.fromtimestamp(data['createdAt'])
+        delete = datetime.fromtimestamp(data['deletedAt'])
+        modify = datetime.fromtimestamp(data['lastModified'])
+
+        target = None if data['duedate'] is None else datetime.strptime(data['duedate'], '%Y-%m-%dT%H:%M:%S%z')
+
+        assignees = [self.__parse_user(d['participant']) for d in data['assignedUsers']]
+        owner = self.__parse_user(data['owner'])
+        editor = None if data['lastEditor'] is None else self.__parse_user(data['lastEditor'])
+
+        order = data['order']
+        archived = data['archived']
+        unread = data['commentsUnread']
+        overdue = data['overdue']
+
+        return DeckCard(cid, tag, title, descr, labels, ctype, attachments, create, modify, delete, target, assignees,
+                        owner, editor, order, archived, unread, overdue)
+
+    def __parse_cards(self, data: list):
+        return [self.__parse_card(d) for d in data]
+
+    def __parse_stack(self, data: dict):
+        sid = data['id']
+        tag = data['ETag']
+        order = data['order']
+        modify = datetime.fromtimestamp(data['lastModified'])
+        title = data['title']
+        cards = None if 'cards' not in data else self.__parse_cards(data['cards'])
+
+        return DeckStack(sid, tag, modify, order, title, cards)
+
+    def __parse_stacks(self, data: list):
+        return [self.__parse_stack(d) for d in data]
+
+    def __parse_board(self, board_json: dict) -> DeckBoard:
+        bid = board_json['id']
+        tag = board_json['ETag']
+
+        permissions = list(board_json['permissions'].values())
+
+        notifications = board_json['settings']['notify-due']
+        calendar = board_json['settings']['calendar']
+
+        title = board_json['title']
+        color = board_json['color']
+        labels = board_json['labels']
+
+        # ACL and owner of the board
+        acl = self.__parse_acls(board_json['acl'])
+        owner = self.__parse_user(board_json['owner'])
+
+        archived = board_json['archived']
+        shared = board_json['shared']
+
+        delete = datetime.fromtimestamp(board_json['deletedAt'])
+        modify = datetime.fromtimestamp(board_json['lastModified'])
+
+        stacks = self.__parse_stacks(json.JSONDecoder().decode(self.__api_request(f'boards/{bid}/stacks')))
+
+        return DeckBoard(bid, tag, permissions, stacks, calendar, title, shared, acl, archived, color, labels, owner,
+                         delete, modify, notifications)
+
+    def download(self):
+        decoder = json.JSONDecoder()
+
+        boards = decoder.decode(self.__api_request('boards'))
+        for b in boards:
+            if not self.__is_outdated(b['title'], b['lastModified']) or b['deletedAt'] != 0 or b['title'] == 'Personal':
+                continue
+
+            self.boards[b['ETag']] = self.__parse_board(b)
+
+    def get_all_cards(self) -> list:
+        result = []
+        for board in self.boards.values():
+            for stack in board.stacks.values():
+                result += list(stack.cards.values())
+        return result
+
+    def get_all_stacks(self) -> list:
+        result = []
+        for board in self.boards.values():
+            result += list(board.stacks.values())
+        return result
+
+
+def main():
+    hostname = os.environ.get("OC_DECK_HOST")
+    username = os.environ.get('OC_DECK_USER')
+    password = os.environ.get('OC_DECK_PASS')
+    security = os.environ.get('OC_USE_HTTPS') == 'True'
+
+    dw = Deck(hostname, username, password, security)
+    pass
+
+
+if __name__ == '__main__':
+    main()
